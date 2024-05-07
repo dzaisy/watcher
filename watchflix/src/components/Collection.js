00ac0a6eba94ceac3c9c534e238786e936ea8713
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-05-07 20:09:37 +0300

    more changes

--- watchflix/src/components/Collection.js
+++ watchflix/src/components/Collection.js
@@ -6,24 +6,24 @@ function Collection({movies, series}) {
     console.log('Movies:', movies);
     console.log('Series:', series);
   
-    const movieCards = movies.map(movie => (
-        <MovieCard 
-            key={movie.id}
-            movie={movie}
-        />
-    ))
+    // const movieCards = movies.map(movie => (
+    //     <MovieCard 
+    //         key={movie.id}
+    //         movie={movie}
+    //     />
+    // ))
 
-    const seriesCards = series.map(series => (
-        <SeriesCard
-            key={series.id}
-            series={series}
-        />
-    ))
+    // const seriesCards = series.map(series => (
+    //     <SeriesCard
+    //         key={series.id}
+    //         series={series}
+    //     />
+    // ))
 
     return (
         <>
-            <div className="card-container">{movieCards}</div>
-            <div className="card-container">{seriesCards}</div>
+            <div className="card-container">{movies}</div>
+            <div className="card-container">{series}</div>
         </>
     )
 }
